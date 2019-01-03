adbbdf883f80523441e3aa56c92c5254d48809f5
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2019-01-03 15:25:11 +0100

    Running Minimal React Application

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const app = props => {
+    return <p>Hello React!</p>
+};
+export default app;
---
+++ webpack.dev.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    }
+};
